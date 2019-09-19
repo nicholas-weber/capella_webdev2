3f1229461d641119a2b860c019ba94451e433b87
Nikko Weber <nikkoweber@Nikkos-MacBook-Pro.local>
2019-09-19 14:17:05 -0500

    removed strange code

--- photos.js
+++ photos.js
@@ -21,20 +21,20 @@ function findPhotos() {
     });
 
     //get board info
-    var pins = [];
-    PDK.request('/v1/me/', function (response) {
-      if (!response || response.error) {
-        //alert('Error occurred');
-      } else {
-        console.log(JSON.stringify(response));
-          //  alert('success');
-            console.log(PDK.getSession().accessToken);
+    // var pins = [];
+    // PDK.request('/v1/me/', function (response) {
+    //   if (!response || response.error) {
+    //     //alert('Error occurred');
+    //   } else {
+    //     console.log(JSON.stringify(response));
+    //       //  alert('success');
+    //         console.log(PDK.getSession().accessToken);
 
-            // var yahoo = $( "#result" ).load( "https://api.pinterest.com/v1/me/?access_token="+PDK.getSession().accessToken+"&fields=counts" );
-            // console.log(yahoo);
-            // PDK.logout();
-      }
-    });
+    //         // var yahoo = $( "#result" ).load( "https://api.pinterest.com/v1/me/?access_token="+PDK.getSession().accessToken+"&fields=counts" );
+    //         // console.log(yahoo);
+    //         // PDK.logout();
+    //   }
+    // });
 
     boardSearch();
 }
